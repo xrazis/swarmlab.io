90306eb15737f0c259ee744ae7043472e930b7a2
Haris Razis <dev@xrazis.com>
2020-10-11 22:30:15 +0300

    ➕ Add signin/up routes
    Does not forwards request / REST

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/documentation', (req, res, next) => {
+router.get('/documentation', (req, res) => {
   res.render('documentation');
 });
 
-router.get('/faq', (req, res, next) => {
+router.get('/faq', (req, res) => {
   res.render('faq');
 });
+
+router.get('/login', (req, res) => {
+  res.send('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.send('signup');
+});
+
 
 module.exports = router;
